427611fbc80492d3a37bddf0a79e0f354e56ed1f
thaddee tyl <thaddee.tyl@gmail.com>
2011-02-05 17:18:13 +0100

    diff.js with a much improved conflict solver.

--- Editor/js/diff.js
+++ Editor/js/diff.js
@@ -68,6 +68,14 @@ alert (JSON.stringify(applydelta (applydeltatest, "Hi ! hello, here!")));
 */
 
 
+/* Helper function. */
+Array.prototype.insert = function (idx, el) {
+  var toend = this.slice (idx);
+  for (var i = idx; i < this.length; i++) { this.pop (); }
+  this.push (el);
+  for (var i = 0; i < toend.length; i++) { this.push (toend[i]); }
+};
+
 /* solve (delta = [[0,12,5], [1,"hello",5]], newdelta = [[1,"ps",0],[1,".",2]])
  * returns an updated version of delta with solved conflicts.
  * Here, [[0,12,8], [1,"hello",8]]. */
@@ -81,14 +89,67 @@ var solve = function (delta, newdelta) {
 
         case 0:  /* Deletion. */
           if (nd[2] < delta[j][2]) {
-            var distfromdel = delta[j][2] - (nd[1] + nd[2]);
-            delta[j][2] -= (distfromdel > 0? nd[1]: distfromdel);
+            if (nd[1] < delta[j][2] - nd[2]) {
+              delta[j][2] -= nd[1];
+            } else {
+              if (delta[j][0] === 1) {
+                /* We inserted something on a spot that was deleted. */
+                nd[1] += delta[j][1].length;  /* Delete it all first. */
+                delta[j][2] = nd[2];  /* Then insert at first position. */
+                i++;
+                newdelta.insert (i, delta[j]);
+              } else {
+                /* We deleted something on a spot that was deleted. */
+                if (delta[j][2] + delta[j][1] < nd[2] + nd[1]) {
+                  /* All that we deleted was already deleted. */
+                  delete delta[j];
+                } else {
+                  nd[1] += (delta[j][2] + delta[j][1]) - (nd[2] + nd[1]);
+                  delete delta[j];
+                }
+              }
+            }
+
+          } else {
+            switch (delta[j][0]) {
+
+              case 0:  /* We deleted on the left of the deletion. */
+                if (delta[j][2] + delta[j][1] < nd[2]) {
+                  nd[2] -= delta[j][1];
+                } else {
+                  /* We deleted past the start of their deletion. */
+                  delta[j][1] -= (delta[j][2] + delta[j][1]) - nd[2];
+                  nd[2] -= delta[j][1];
+                }
+                break;
+
+              case 1:  /* We inserted on the left. */
+                nd[2] += delta[j][1].length;
+                break;
+            }
           }
+
           break;
 
         case 1:  /* Insertion. */
-          if (nd[2] < delta[j][2]) {
+          if (nd[2] <= delta[j][2]) {
             delta[j][2] += nd[1].length;
+          } else {
+            /* We act on the left of the insertion. */
+            if (delta[j][0] === 0) {
+              if (delta[j][1] < nd[2] - delta[j][2]) {
+                nd[2] -= delta[j][1];
+              } else {
+                  /* They inserted something on a spot that was deleted. */
+                  delta[j][1] += nd[1].length;  /* We delete it all first. */
+                  nd[2] = delta[j][2];/* Then we insert at the first position.*/
+                  j++;
+                  delta.insert (j, nd);
+              }
+            } else {
+              /* They inserted something on a spot that was an insertion. */
+              nd[2] += delta[j][1].length;
+            }
           }
           break;
 
@@ -107,8 +168,8 @@ alert (JSON.stringify(solve (solvetest, [[1,"ps",0],[1,".",2]])));
 
 
 window.Diff = {
-  diff: diff,
-  applydiff: applydiff,
+  delta: delta,
+  applydelta: applydelta,
   solve: solve
 };
 
