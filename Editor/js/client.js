b973f828ca8f8bbe0b20553e3a06cd4f8b0f5909
jan333 <jan.keromnes@gmail.com>
2011-02-08 22:53:15 +0100

    Yann bug correction

--- Editor/js/client.js
+++ Editor/js/client.js
@@ -4,4 +4,29 @@
 
 (function (){
   
+  
+  
+  var editor = new CodeMirror(document.body, {
+      content: window.client.copy,
+      height: "50%",
+      width: "100%",
+      parserfile: ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js", "parsehtmlmixed.js"],
+      stylesheet: ["css/xmlcolors.css", "css/jscolors.css", "css/csscolors.css"],
+      path: "js/"
+    });
+	
+	
+    setInterval(Scout.send(function(xhr, params){
+      params.data = {
+	    usr: window.client.usr,
+        rev: window.client.rev,
+        delta: window.client.delta
+      };
+      params.open.url = '';
+      params.resp = function(xhr, resp) {
+        alert(JSON.stringify(resp));
+      };
+    }), window.client.timeout);
+  
+  
 })()
