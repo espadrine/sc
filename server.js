f511b1020c2cfd19c48169b057f4c489fd0ecb85
jankeromnes <jan.keromnes@gmail.com>
2011-08-10 01:23:15 +0200

    Worked on some demos.

--- server.js
+++ server.js
@@ -2,17 +2,30 @@
  * Copyright (c) 2011 Jan Keromnes, Thaddee Tyl. All rights reserved.
  * Code covered by the LGPL license. */
 
-// import the Camp server module for easy web & ajax
+// import the Camp
 var Camp = require ('./lib/camp.js');
 
-// templating information of the index page
-Camp.handle ('/index.html', function (query, path) {
-  var data = {
-     title: query.title || 'Success',
-     h1: query.h1 || 'Success!',
-     p: query.p || 'You\'re on the web!'
+var replies = ['Ok.', 'Oh⁉', 'Is that so?', 'How interesting!', 'Hm…', 'So say we all.']
+
+// handle ajax
+Camp.add('doctor', function (data) {
+  replies.push (data.text);
+  return { reply: replies [ Math.floor ( Math.random() * replies.length ) ] };
+});
+
+// handle templating
+Camp.handle ('/index.html', function (data, path) {
+  var map = {
+     title: data.title || 'Success',
+     h1: data.h1 || 'Success!',
+     p: data.p || 'You\'re on the web!'
   };
-  return data;
+  return map;
+});
+
+// handle not found
+Camp.notfound(/.*/, function (data, path) {
+  path[0] = '/404.html';
 });
 
 // let's rock'n'roll!
