0697f3d51dfdda0711695067bd2fa958513b91c2
jankeromnes <jan.keromnes@gmail.com>
2011-08-11 20:55:47 +0200

    Worked on chatter demo.

--- server.js
+++ server.js
@@ -2,28 +2,38 @@
  * Copyright (c) 2011 Jan Keromnes, Thaddee Tyl. All rights reserved.
  * Code covered by the LGPL license. */
 
-// import the Camp
+
+// Import the Camp
 var Camp = require ('./lib/camp.js');
 
-var replies = ['Ok.', 'Oh⁉', 'Is that so?', 'How interesting!', 'Hm…', 'So say we all.']
 
-// handle ajax
+// Templating demo
+Camp.handle('/template.html', function (data, path) {
+  var map = {
+     title: data.title || 'Success',
+     enc: data.enc || 'utf-8',
+     info: data.info || 'This document has been templated!'
+  };
+  return map;
+});
+
+// doctor ajax demo
+var replies = ['Ok.', 'Oh⁉', 'Is that so?', 'How interesting!', 'Hm…', 'So say we all.']
 Camp.add('doctor', function (data) {
   replies.push (data.text);
   return { reply: replies [ Math.floor ( Math.random() * replies.length ) ] };
 });
 
-// handle templating
-Camp.handle ('/index.html', function (data, path) {
-  var map = {
-     title: data.title || 'Success',
-     h1: data.h1 || 'Success!',
-     p: data.p || 'You\'re on the web!'
-  };
-  return map;
+// chat demo
+var buffer = [];
+Camp.add('talk', function(data){
+  buffer.push(data.message);
+});
+Camp.add('all', function(data){
+  // soup, guys
 });
 
-// handle not found
+// not found demo
 Camp.notfound(/.*/, function (data, path) {
   path[0] = '/404.html';
 });
