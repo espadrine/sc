b0759a52e74ed305f3647b3860873079f6b78dea
Jan Keromnes <janx@linux.com>
2016-10-12 17:03:26 +0000

    Test res.json(data)

--- test/test-api.js
+++ test/test-api.js
@@ -1,6 +1,7 @@
 var camp = require('../lib/camp');
 var fleau = require('fleau');
 var http = require('http');
+var mime = require('../lib/mime.json');
 var Test = require('./test');
 var t = new Test();
 
@@ -66,19 +67,19 @@ var launchTests = function() {
         res.end('baz');
       });
 
-      get('/foo', function(res) {
-        res.on('data', function(content) {
-          t.eq(String(content), 'foo',
+      get('/foo', function (res) {
+        res.on('data', function (body) {
+          t.eq(String(body), 'foo',
             'Basic sc.path should work.');
 
-          get('/foo/quux', function(res) {
-            res.on('data', function(content) {
-              t.eq(String(content), 'bar=quux',
+          get('/foo/quux', function (res) {
+            res.on('data', function (body) {
+              t.eq(String(body), 'bar=quux',
                 'Named sc.path placeholder should work.');
 
-              get('/foo/quux/baz', function(res) {
-                res.on('data', function(content) {
-                  t.eq(String(content), 'baz',
+              get('/foo/quux/baz', function (res) {
+                res.on('data', function (body) {
+                  t.eq(String(body), 'baz',
                     'Named sc.path placeholder should not pre-empt sub-paths.');
                   next();
                 });
@@ -87,6 +88,27 @@ var launchTests = function() {
           });
         });
       }); // Mmmh… I love spaghetti!
+    },
+
+    function t3(next) {
+      var data = { message: '☃' };
+
+      server.path('json', function (req, res) {
+        res.statusCode = 418; // I'm a teapot (see RFC 2324).
+        res.json(data);
+      });
+
+      get('/json', function (res) {
+        t.eq(res.statusCode, 418,
+          'Setting `res.statusCode` before `res.json(data)` should work.');
+        t.eq(res.headers['content-type'], mime.json,
+          'Served content type should always be "' + mime.json + '".')
+        res.on('data', function (body) {
+          t.eq(String(body).trim(), JSON.stringify(data, null, 2),
+            '`res.json(data)` should return human-readable JSON.');
+          next();
+        });
+      });
     }
   ], function end() {
     t.tldr();
