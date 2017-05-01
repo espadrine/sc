bc2263a0cb5c0868d9e136bef422f7b4930bde19
Thaddee Tyl <thaddee.tyl@gmail.com>
2017-05-01 10:16:47 +0200

    Update app.js to new APIs
    The API changes were driven by what we wanted to be able to use, with
    an emphasis on control over what happens in HTTP and in the Node.js
    libraries. We wrote the kind of API we wanted in the following gist:
    https://gist.github.com/espadrine/909be38537dd06d3a4dc

--- app.js
+++ app.js
@@ -1,64 +1,52 @@
-// Server demo. Run this with node to start your server.
-// Copyright © 2011-2015 Thaddee Tyl, Jan Keromnes. All rights reserved.
-// Code covered by the LGPL license.
+// Server demo. `node app [PORT [https]]`
+// © 2011-2017 Thaddée Tyl, Jan Keromnes. LGPL.
 
-// Let's rock'n'roll!
-var port = +process.argv[2] || +process.env.PORT || 1234;
-var camp = require('./lib/camp.js').start({
-      port: port,
-      secure: process.argv[3] === 'secure',
-    })
-  , ajax = camp.ajax
-console.log('http://[::1]:' + port);
+const port = +process.argv[2] || +process.env.PORT || 1234
+let camp = require('./lib/camp.js')
+let sc = camp.start({port: port, secure: process.argv[3] === 'https'})
+console.log('http://[::1]:' + port)
 
-// Templating demo
-camp.path('template.html');
+// Templating demo: /template.html?title=Hello&info=[Redacted].
+sc.path('/template.html')
 
 // Templating demo with multiple templates and path parameter.
 // /html.template/Hello/World
-let flip = camp.template(['web/template.html', 'web/flip.html'])
-camp.path('html.template/:title/:info', function(req, res) {
-  res.template(req.data, flip);
-});
+let flip = sc.template(['web/template.html', 'web/flip.html'])
+sc.path('/html.template/:title/:info', (req, res) => {
+  res.template(req.data, flip)
+})
 
-// Doctor demo
-var replies = ['Ok.', 'Oh⁉', 'Is that so?', 'How interesting!'
-              ,'Hm…', 'What do you mean?', 'So say we all.']
-ajax.on('doctor', function(data, end) {
-  replies.push(data.text)
-  end({reply:replies[Math.floor(Math.random() * replies.length)]})
+// Doctor demo: /doctor?text=…
+let replies = ['Ok.', 'Oh⁉', 'Is that so?', 'How interesting!',
+               'Hm…', 'What do you mean?', 'So say we all.']
+sc.post('/doctor', (req, res) => {
+  replies.push(req.query.text)
+  res.json({reply: replies[Math.random() * replies.length|0]})
 })
 
 // Chat demo
-var chat = camp.eventSource('all')
-ajax.on('talk', function(data, end) {chat.send(data); end()})
+let chat = sc.eventSource('/all')
+sc.post('/talk', (req, res) => {chat.send(req.data); res.end()})
 
 // WebSocket chat demo
-camp.wsBroadcast('chat', function(data, end) {end(data)})
+sc.wsBroadcast('/chat', (req, res) => res.send(req.data))
 
 // Not found demo
-camp.notFound('*.lol', function(req, res) { res.file('/404.html') })
+sc.notFound('/*.lol', (req, res) => res.file('/404.html'))
 
-// Basic authentication demo.
-camp.path('secret', function(req, res) {
+// Basic authentication demo
+sc.get('/secret', (req, res) => {
   if (req.username === 'Caesar' && req.password === '1234') {
-    res.end('Congrats, you found it!');
+    res.end('Congrats, you found it!')
   } else {
-    res.statusCode = 401;
-    res.setHeader('WWW-Authenticate', 'Basic');
-    res.end('Nothing to hide here!');
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic')
+    res.end('Nothing to hide here!')
   }
 })
 
 // Low-level handler
-camp.handle(function(req, res, down) {
+sc.handle((req, res, down) => {
   res.setHeader('X-Open-Source', 'https://github.com/espadrine/sc/')
   down()
 })
-
-var stream = require('stream')
-function streamFromString(str) {
-  var newStream = new stream.Readable()
-  newStream._read = function() { newStream.push(str); newStream.push(null) }
-  return newStream
-}
